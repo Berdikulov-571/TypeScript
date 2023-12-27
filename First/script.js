9d20cb357ddfd760ec409be9cc8d15535a6c611b
berdikulov-571 <mygithub571@gmail.com>
2023-12-27 11:29:25 +0500

    script.js

--- First/script.js
+++ First/script.js
@@ -1,15 +1,56 @@
-// function Sum(a : number, b : number) : void
-// {
-//     console.log(a + b);
+// type User = {
+//     firstName : string,
+//     lastName : string,
+//     phoneNumber: string,
+//     Age : number
 // }
-var person = {
-    firstName: "Sanjarbek",
-    lastName: "Berdikulov",
-    Age: 17,
-    phoneNumber: "900074272",
-    address: "Bekobod",
-};
-function getPersonInfo(person) {
-    return "FirstName : ".concat(person.firstName, "\nLastName: ").concat(person.lastName, "\nPhoneNumber: ").concat(person.phoneNumber, "\nAge: ").concat(person.Age, "\nAddress: ").concat(person.address);
-}
-console.log(getPersonInfo(person));
+// function GetFullInfo(user : User) : string
+// {
+//     return `FirstName : ${user.firstName}\nLastName: ${user.lastName}\nPhoneNumber: ${user.phoneNumber}\nAge: ${user.Age}`;
+// }
+// let user : User =
+// {
+//     firstName : "Sanajrbek",
+//     lastName : "Berdikulov",
+//     Age : 17,
+//     phoneNumber : "+998900074272",
+// }
+// console.log(GetFullInfo(user));
+// interface IUser {
+//     firstName: string,
+//     lastName: string,
+//     phoneNumber: string,
+//     Age: number
+// }
+// interface IPerson extends IUser {
+//     address: string,
+// }
+// let person: IPerson =
+// {
+//     firstName: "Sanjarbek",
+//     lastName: "Berdikulov",
+//     Age: 17,
+//     phoneNumber: "900074272",
+//     address: "Bekobod",
+// }
+// function getPersonInfo(person: IPerson): string {
+//     return `FirstName : ${person.firstName}\nLastName: ${person.lastName}\nPhoneNumber: ${person.phoneNumber}\nAge: ${person.Age}\nAddress: ${person.address}`;
+// }
+// console.log(getPersonInfo(person));
+// type Person = {
+//     firstName : string,
+//     lastName : string,
+//     phoneNumber: string,
+//     Age : number
+// }
+// type User = Person & {
+//     Address : string,
+// }
+// let user : User = {
+//     firstName: "Sanjarbek",
+//     lastName: "Berdikulov",
+//     Age: 17,
+//     phoneNumber: "900074272",
+//     Address: "Bekobod",
+// }
+// console.log(user.Address);
